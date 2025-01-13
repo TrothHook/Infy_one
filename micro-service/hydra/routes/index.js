8cd90a1595d13ff08c8e813bb9ac1f79dc1caa50
TrothHook <HrithikSarkar45@protonmail.com>
2025-01-13 22:05:00 +0530

    load balancing

--- micro-service/hydra/routes/index.js
+++ micro-service/hydra/routes/index.js
@@ -1,9 +1,10 @@
 const he = require("hydra-express");
 const express = he.getExpress();
 const router = express.Router();
+const hydra = he.getHydra();
 
 router.get("/greeting", (req, res, next) => {
-  res.json({ msg: "hello world" });
+  res.json({ msg: "hello world" + hydra.getInstanceID() });
 });
 
 module.exports = router;
